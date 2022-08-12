b7ab073d16d78c6518988086b825824c0a9064fb
joao966 <joaooliveira966@gmail.com>
2022-08-12 18:04:06 -0300

    ADD: dto

--- src/produtos/dto/create-produto.dto.ts
+++ src/produtos/dto/create-produto.dto.ts
@@ -1,7 +1,16 @@
+import { IsNotEmpty } from 'class-validator';
+import { Expose } from 'class-transformer';
+
 export class CreateProdutoDto {
+  @IsNotEmpty()
   nome: string;
+  @IsNotEmpty()
+  @Expose()
   valor: number;
+  @IsNotEmpty()
   quantidade: number;
+  @IsNotEmpty()
   ml: number;
+  @IsNotEmpty()
   tipo: string;
 }
