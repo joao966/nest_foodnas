ae41959dc5d657cae35e15c039a6aa0bc33f0caf
joao966 <joaooliveira966@gmail.com>
2022-08-12 14:08:28 -0300

    ADD: modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosModule } from './produtos/produtos.module';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [ProdutosModule],
+  imports: [ProdutosModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
