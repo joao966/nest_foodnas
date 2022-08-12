4aad9663c68760dc942ed4e9028d284c48cc299d
joao966 <joaooliveira966@gmail.com>
2022-08-12 14:08:58 -0300

    ADD: entidade

--- src/produtos/entities/produto.entity.ts
+++ src/produtos/entities/produto.entity.ts
@@ -1,8 +1,19 @@
-export class Produto {
-  id: number;
+import { Table, Column, Model } from 'sequelize-typescript';
+
+@Table
+export class produto extends Model {
+  @Column
   nome: string;
+  
+  @Column
   valor: number;
+  
+  @Column
   quantidade: number;
+  
+  @Column
   ml: number;
+  
+  @Column
   tipo: string;
 }
