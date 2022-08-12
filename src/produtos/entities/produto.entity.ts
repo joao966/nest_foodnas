f7bd93fda6b6ab624c96c7faa52405e7dd24c507
joao966 <joaooliveira966@gmail.com>
2022-08-12 18:04:15 -0300

    ADD: entidade

--- src/produtos/entities/produto.entity.ts
+++ src/produtos/entities/produto.entity.ts
@@ -1,7 +1,8 @@
 import { Table, Column, Model } from 'sequelize-typescript';
+import { CreateProdutoDto } from '../dto/create-produto.dto';
 
 @Table
-export class produto extends Model {
+export class produto extends Model<CreateProdutoDto> {
   @Column
   nome: string;
   
