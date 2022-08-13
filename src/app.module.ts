9f2e6dd8f13a8c758145669c95c886dc0078fe4d
joao966 <joaooliveira966@gmail.com>
2022-08-13 19:17:46 -0300

    ADD: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosModule } from './produtos/produtos.module';
 import { DatabaseModule } from './database.module';
+import { RegistersModule } from './registers/registers.module';
 
 @Module({
-  imports: [ProdutosModule, DatabaseModule],
+  imports: [ProdutosModule, DatabaseModule, RegistersModule],
   controllers: [AppController],
   providers: [AppService],
 })
