76ac821396281e5c43bf84ba4493f388eccc7f53
esionascimento <esionascimento@protonmail.com>
2022-08-14 02:14:50 -0400

    chore: adicionado dotenv db

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 import { produto } from './produtos/entities/produto.entity';
 import { registers } from './registers/entities/register.entity';
+import 'dotenv/config';
 
 export const databaseProviders = [
   {
@@ -8,11 +9,17 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'password',
-        database: 'food_nas',
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: process.env.DB_SSL
+          }
+        }
       });
       sequelize.addModels([produto, registers]);
       await sequelize.sync();
