18e51156dde2b700b5ab10c60438b073d201b323
esionascimento <esionascimento@protonmail.com>
2022-08-14 03:33:42 -0400

    refactor: entity produtos, column com type

--- src/produtos/entities/produto.entity.ts
+++ src/produtos/entities/produto.entity.ts
@@ -3,20 +3,34 @@ import { CreateProdutoDto } from '../dto/create-produto.dto';
 
 @Table
 export class produto extends Model<CreateProdutoDto> {
-  @Column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   nome: string;
   
   @Column({
     type: DataType.FLOAT,
+    allowNull: false,
   })
   valor: number;
   
-  @Column
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
   quantidade: number;
   
-  @Column
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
   ml: number;
   
-  @Column
+  @Column({
+    type: DataType.STRING,
+    values: ['unidade', 'caixa'],
+    allowNull: false,
+  })
   tipo: string;
 }
