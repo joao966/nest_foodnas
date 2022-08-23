09a5b739c93d78372400ce12f239ecd50d8d4f0c
joao966 <joaooliveira966@gmail.com>
2022-08-23 14:58:31 -0300

    ADD: create basic structing

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { AppService } from './app.service';
 import { ProdutosModule } from './modules/produtos/produtos.module';
 import { DatabaseModule } from './database.module';
 import { RegistersModule } from './modules/registers/registers.module';
+import { EstoquesModule } from './modules/estoques/estoques.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ProdutosModule, DatabaseModule, RegistersModule
+    ProdutosModule, DatabaseModule, RegistersModule, EstoquesModule
   ],
   controllers: [AppController],
   providers: [AppService],
