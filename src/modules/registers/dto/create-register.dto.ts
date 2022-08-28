0481c43b94816d81dfad69acb78215c1b1ce3194
esionascimento <esionascimento@proton.me>
2022-08-28 13:42:01 -0400

    refactor: add @ApiProperty do post registers

--- src/modules/registers/dto/create-register.dto.ts
+++ src/modules/registers/dto/create-register.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateRegistersDto {
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'nome',
+    description: 'Nome de usuário.',
+  })
   nome: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'email@email.com',
+    description: 'Email válido.',
+  })
   email: string;
 
   @MinLength(6)
   @IsNotEmpty()
+  @ApiProperty({
+    example: '******',
+    description: 'Senha com mais de 5 caracteres.',
+  })
   password: number;
 }
