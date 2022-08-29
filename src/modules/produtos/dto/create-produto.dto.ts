d25271e9e043996b5411fa50f122b5ca99775510
esionascimento <esionascimento@proton.me>
2022-08-29 10:10:54 -0400

    refactor: swager add @ApiProperty documentation no create-produto.dto

--- src/modules/produtos/dto/create-produto.dto.ts
+++ src/modules/produtos/dto/create-produto.dto.ts
@@ -1,5 +1,6 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 enum Tipo {
   unidade = 'unidade',
@@ -8,21 +9,41 @@ enum Tipo {
 
 export class CreateProdutoDto {
   @IsNotEmpty()
+  @ApiProperty({
+    example: '',
+    description: 'Codigo de barra do produto, unitario ou caixa',
+  })
   codigo_barra: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: '',
+    description: 'Nome do produto.',
+  })
   nome: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: '7.98',
+    description: 'Valor do produto. (usar ponto)',
+  })
   @Expose()
   valor: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: '1000',
+    description: 'ML do produto. (Quantidade em ml)',
+  })
   ml: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'unidade',
+    description: `Tipo do produto, 'unidade' ou 'caixa'`,
+  })
   @IsEnum(Tipo, {
-    message: 'Tipo must be either unidade or caixa',
+    message: 'Tipo só aceita unidade ou caixa',
   })
   tipo: string;
 }
