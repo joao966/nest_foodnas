b7fd12520cf7924074a6908efdc5bf76f25ca859
esionascimento <esionascimento@proton.me>
2022-08-29 10:09:04 -0400

    refactor: add @ApiProperty documentation no create-estoque.dto

--- src/modules/estoques/dto/create-estoque.dto.ts
+++ src/modules/estoques/dto/create-estoque.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateEstoqueDto {
   @IsNotEmpty()
+  @ApiProperty({
+    example: '1',
+    description: 'Quantidade de produtos.',
+  })
   quantidade_total_caixa: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: '',
+    description: '',
+  })
   quantidade_total_unidade: boolean;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: '',
+    description: 'Codigo de barra de identificação do produto.',
+  })
   codigo_barra: string;
 }
